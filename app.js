a1de0550e49919b6a770e915482d6a207e0d51ae
macarena <macamonper@gmail.com>
2021-11-08 22:02:22 -0300

    agregar categorias
    agregar categorias y que aparezcan en HTML

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const BALANCE= document.getElementById ("btn-balance");
-const CATEGORIAS= document.getElementById("btn-categorias");
+const BTN_CATEGORIAS= document.getElementById("btn-categorias");
 const REPORTES= document.getElementById("btn-reportes");
 const CARD_BALANCE= document.getElementById("card-balance");
 const CARD_CATEGORIAS= document.getElementById("card-categorias");
 const CARD_REPORTES= document.getElementById("card-reportes");
 const BTN_FILTROS= document.getElementById("btn-filtros");
 const CARD_FILTROS = document.getElementById("card-filtros");
+const BTN_AGREGAR_CAT = document.getElementById("btn-agregar-cat");
+const INPUT_CATEGORIAS = document.getElementById("input-categorias");
 
-
+//FUNCIONES BASICAS PARA NAVEGAR LA WEB
 BALANCE.onclick = () => {
     CARD_CATEGORIAS.classList.add("is-hidden");
     CARD_REPORTES.classList.add("is-hidden");
     CARD_BALANCE.classList.remove("is-hidden");
  }
  
-CATEGORIAS.onclick = () => {
+BTN_CATEGORIAS.onclick = () => {
     CARD_BALANCE.classList.add("is-hidden");
     CARD_REPORTES.classList.add("is-hidden");
     CARD_CATEGORIAS.classList.remove("is-hidden");
@@ -36,5 +38,50 @@ BTN_FILTROS.onclick = () => {
         CARD_FILTROS.classList.add("is-hidden");
     }
 }
+//AGREGAR CATEGORIAS
 
-    
+const categorias = ["Alimentos", "Mascotas", "Sueldo", "Transporte"];
+
+    const obtenerCategorias = () =>{
+        const catEnLS = localStorage.getItem("categorias")
+
+        if (catEnLS === null){
+            return categorias
+        
+        }
+        else{
+        return JSON.parse (catEnLS)
+        }
+    }
+
+const AgregarCategoriasAlSelect = () =>{
+    const categorias = obtenerCategorias()
+
+    const categoriasString = categorias.reduce ((acc,elemento) => {
+        return acc + `<option value="${elemento}">${elemento}</option>`
+    },"")
+
+    INPUT_CATEGORIAS.innerHTML = categoriasString
+}
+
+const agregarCategoriasHTML = () => {
+    const categorias = obtenerCategorias()
+    const lista = document.querySelector("#lista-categorias")
+
+    const categoriasString = categorias.reduce((acc,elemento,index) =>{
+        return acc + `
+        <div id="lista-categorias" class="columns">
+            <div class="column is-9">
+                <p class="tag is-primary is-light "> ${elemento}</p>
+            </div>
+            <div class="column is-1 has-text-right">
+                <button class="button is-ghost is-size-7 m-0">Editar</button>
+
+            </div>
+
+            <div class="column is-2 ">
+                <button class="button is-ghost  is-size-7">Eliminar</button>
+
+            </div>`
+    },"")
+}
